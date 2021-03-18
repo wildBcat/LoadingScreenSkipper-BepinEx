772dbbb7fd477c972d1f5c0c982e9caa94b2d391
18107 <18107@users.noreply.github.com>
2021-03-18 15:07:29 +1100

    Add project files.

---
+++ LoadingScreenSkipper/IntroUpdate.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+
+namespace LoadingScreenSkipper
+{
+    [HarmonyPatch(typeof(PLIntro), "Update")]
+    class IntroUpdate
+    {
+        static void Prefix(PLIntro __instance, ref float ___LifeTime, ref bool ___LevelLoadRequestMade)
+        {
+            if (!___LevelLoadRequestMade)
+            {
+                ___LevelLoadRequestMade = true;
+                ___LifeTime = 11f;
+                PLGlobal.IntroFinished = true;
+                PLLoader.Instance.LoadThis(new PLLoadRequest(PLGlobal.MainMenuLevelID));
+            }
+        }
+    }
+}
---
+++ LoadingScreenSkipper/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using PulsarPluginLoader;
+
+namespace LoadingScreenSkipper
+{
+    public class Plugin : PulsarPlugin
+    {
+        public override string Version => "0.0.0";
+
+        public override string Author => "18107";
+
+        public override string ShortDescription => "Skips the cutscene at the start of the game";
+
+        public override string Name => "Cutscene Skipper";
+
+        public override string HarmonyIdentifier()
+        {
+            return "mod.id107.cutsceneskipper";
+        }
+    }
+}
